4c48385b9467cc5dddcbcaca33c31b659f0780eb
Carolina Ferreira <carolina.ferreira@poatek.com>
2024-09-09 18:21:35 -0300

    test implementing lazy load

--- trivia-app/src/components/Header/index.tsx
+++ trivia-app/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Header() {
 	return (
 		<Flex className="justify-center items-center gap-1 z-10">
-			<Image className="w-10" src="/images/puzzle.svg" alt="Trivia App" />
+			<Image width={24} height={24} src="/images/puzzle.svg" alt="Trivia App" />
 			<h1 className="text-xl font-bold">Trivia App</h1>
 		</Flex>
 	);
--- trivia-app/src/components/QuizCard/index.tsx
+++ trivia-app/src/components/QuizCard/index.tsx
@@ -1,17 +1,55 @@
 import { Box } from "@radix-ui/themes";
+import { useEffect, useRef, useState } from "react";
 
 interface QuizCardProps {
-  category?: string;
-  backgroundImage: string;
-  onClick?: () => void;
+	category?: string;
+	backgroundImage: string;
+	onClick?: () => void;
 }
 
 export default function QuizCard(props: QuizCardProps) {
-  return (
-    <Box className="w-44 h-44 relative z-0 bg-cover bg-center rounded-lg shadow-lg" style={{ backgroundImage: `url(${props.backgroundImage})` }} onClick={props.onClick}>
-      <Box className="w-3/4 h-1/4 p-4 bg-gray-200 absolute z-10 bg-transparent absolute top-14 right-6">
-        <h2 className="text-base font-bold text-center text-white shadow-xs">{props.category}</h2>
-      </Box>
-    </Box>
-  )
+	const [isInView, setIsInView] = useState(false);
+	const cardRef = useRef<HTMLDivElement>(null);
+
+	//Implementing lazy load to improve img loading performance
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				if (entry.isIntersecting) {
+					setIsInView(true);
+					observer.disconnect();
+				}
+			},
+			{
+				rootMargin: "100px",
+			}
+		);
+
+		if (cardRef.current) {
+			observer.observe(cardRef.current);
+		}
+
+		return () => {
+			if (cardRef.current) {
+				observer.unobserve(cardRef.current);
+			}
+		};
+	}, []);
+
+	return (
+		<Box
+			ref={cardRef}
+			className="w-44 h-44 relative z-0 bg-cover bg-center rounded-lg shadow-lg"
+			style={{
+				backgroundImage: isInView ? `url(${props.backgroundImage})` : "none", // Only load the background image when it's in view
+			}}
+			onClick={props.onClick}
+		>
+			<Box className="w-3/4 h-1/4 p-4 bg-gray-200 absolute z-10 bg-transparent absolute top-14 right-6">
+				<h2 className="text-base font-bold text-center text-white shadow-xs">
+					{props.category}
+				</h2>
+			</Box>
+		</Box>
+	);
 }
