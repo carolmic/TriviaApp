75b6d8e4296ac7567dda4ab77c9b98105d4ceacb
Carolina Ferreira <carolina.ferreira@poatek.com>
2024-09-09 10:05:33 -0300

    add new quiz categories

--- trivia-app/src/components/QuizCard/index.tsx
+++ trivia-app/src/components/QuizCard/index.tsx
@@ -1,16 +1,16 @@
 import { Box } from "@radix-ui/themes";
 
 interface QuizCardProps {
-  category: string;
+  category?: string;
   backgroundImage: string;
+  onClick?: () => void;
 }
 
 export default function QuizCard(props: QuizCardProps) {
   return (
-    <Box className="w-44 h-96 ">
-      <Box className="w-full h-3/4 bg-cover bg-center" style={{ backgroundImage: `url(${props.backgroundImage})` }} />
-      <Box className="w-full h-1/4 p-4 bg-gray-200">
-        <h2 className="text-lg font-bold">{props.category}</h2>
+    <Box className="w-44 h-44 relative z-0 bg-cover bg-center rounded-lg shadow-lg" style={{ backgroundImage: `url(${props.backgroundImage})` }} onClick={props.onClick}>
+      <Box className="w-3/4 h-1/4 p-4 bg-gray-200 absolute z-10 bg-transparent absolute top-14 right-6">
+        <h2 className="text-base font-bold text-center text-white shadow-xs">{props.category}</h2>
       </Box>
     </Box>
   )
--- trivia-app/src/pages/index.tsx
+++ trivia-app/src/pages/index.tsx
@@ -1,15 +1,76 @@
 import Header from '@/components/Header';
 import QuizCard from '@/components/QuizCard';
-import { Button, Flex } from '@radix-ui/themes';
+import { Flex, Grid } from '@radix-ui/themes';
 import { useRouter } from 'next/router';
+
+const quizzes = [
+  {
+    category: 'General Knowledge',
+    id: 9,
+    backgroundImage: '/general_knowledge.jpg'
+  }, 
+  {
+    category: 'Books',
+    id: 10,
+    backgroundImage: '/books.jpg'
+  }, 
+  {
+    category: 'Films',
+    id: 11,
+    backgroundImage: '/film.jpg'
+  }, 
+  {
+    category: 'Music',
+    id: 12,
+    backgroundImage: '/music.jpg'
+  }, 
+  {
+    category: 'Musicals & Theatres',
+    id: 13,
+    backgroundImage: '/theatre.jpg'
+  },
+  {
+    category: 'Television',
+    id: 14,
+    backgroundImage: '/television.jpg'
+  },
+  {
+    category: 'Video Games',
+    id: 15,
+    backgroundImage: '/video_games.jpg'
+  },
+  {
+    category: 'Board Games',
+    id: 16,
+    backgroundImage: '/board_games.jpg'
+  },
+  {
+    category: 'Science & Nature',
+    id: 17,
+    backgroundImage: '/nature.jpg'
+  },
+  {
+    category: 'Computers',
+    id: 18,
+    backgroundImage: '/computers.jpg'
+  },
+  {
+    category: 'Math',
+    id: 19,
+    backgroundImage: '/mathematics.jpg'
+  }
+];
 
 export default function MyApp() {
   const router = useRouter();
   return (
     <Flex direction="column" gap="2">
       <Header />
-      <Button onClick={() => router.push('/quiz')}>Start Quiz</Button>
-      <QuizCard category="General Knowledge" backgroundImage="https://avatars.githubusercontent.com/u/151643887?v=4" />
+      <Grid columns="4" rows="repeat(2, 15rem)" width="50%">
+        {quizzes.map((quiz, index) => (
+          <QuizCard key={index} category={quiz.category} backgroundImage={quiz.backgroundImage} onClick={() => router.push(`/quiz/${quiz.id}`)}/>
+        ))}
+      </Grid>
     </Flex>
   );
 }
